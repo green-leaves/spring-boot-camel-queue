387caac6cbba4b51fb13266d247ed2a38815ec9b
Nguyen Duy Tiep <duytiep@gmail.com>
2017-06-03 10:37:43 +0800

    init

---
+++ src/main/java/com/greenleaves/spring/queue/SpringBootQueueApplication.java
@@ -0,0 +1,12 @@
+package com.greenleaves.spring.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQueueApplication.class, args);
+	}
+}
